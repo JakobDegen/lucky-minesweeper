ab115ba02fd07bdcb79215f610e1e8f6ce439fb1
Jakob Degen <jakob@degen.com>
2021-10-24 18:47:38 -0400

    Reduce benchmark sizes to get Criterion to be more useful

--- benches/all.rs
+++ benches/all.rs
@@ -3,13 +3,13 @@ use lucky_minesweeper::*;
 
 fn bench_all(c: &mut Criterion) {
     c.bench_function("Beginner", |b| {
-        b.iter(|| test_single_threaded::<9, 9, 10, 100_000, 0>())
+        b.iter(|| test_single_threaded::<9, 9, 10, 10_000, 0>())
     });
     c.bench_function("Intermediate", |b| {
-        b.iter(|| test_single_threaded::<16, 16, 40, 50_000, 0>())
+        b.iter(|| test_single_threaded::<16, 16, 40, 10_000, 0>())
     });
     c.bench_function("Advanced", |b| {
-        b.iter(|| test_single_threaded::<30, 16, 99, 15_000, 0>())
+        b.iter(|| test_single_threaded::<30, 16, 99, 5_000, 0>())
     });
 }
 
